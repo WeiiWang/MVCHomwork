84cde0a18bf80b0d64d144141717a15511fc470d
Tom <alex60510@hotmail.com>
2018-07-17 23:58:44 +0800

    修改客戶聯絡人Email驗證方式

--- WebApplication3/Models/客戶聯絡人.Partial.cs
+++ WebApplication3/Models/客戶聯絡人.Partial.cs
@@ -7,9 +7,22 @@ namespace WebApplication3.Models
 
 
     [MetadataType(typeof(客戶聯絡人MetaData))]
-    public partial class 客戶聯絡人
+    public partial class 客戶聯絡人 : IValidatableObject
     {
+        public 客戶聯絡人Repository repo { get; set; }
+        public 客戶聯絡人()
+        {
+            this.repo = RepositoryHelper.Get客戶聯絡人Repository();
+        }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (repo.EmailDuplicate(Id,客戶Id,Email))
+            {
+                yield return new ValidationResult("Email重複", new[] { "Email" });
+            }
+
+        }
     }
 
     public partial class 客戶聯絡人MetaData
@@ -18,29 +31,28 @@ namespace WebApplication3.Models
         public int Id { get; set; }
         [Required]
         public int 客戶Id { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 職稱 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         [Required]
         public string 姓名 { get; set; }
-        
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+
+        [StringLength(250, ErrorMessage = "欄位長度不得大於 250 個字元")]
         [Required]
         [EmailAddress]
-        [EmailDuplicate]
         public string Email { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         //[RegularExpression(@"\d{4}-\d{6}")]
         [CellPhone]
         public string 手機 { get; set; }
-        
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+
+        [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 電話 { get; set; }
-    
+
         public virtual 客戶資料 客戶資料 { get; set; }
     }
 }
--- WebApplication3/Models/客戶聯絡人Repository.cs
+++ WebApplication3/Models/客戶聯絡人Repository.cs
@@ -2,11 +2,11 @@ using System;
 using System.Linq;
 using System.Linq.Dynamic;
 using System.Collections.Generic;
-	
+
 namespace WebApplication3.Models
-{   
-	public  class 客戶聯絡人Repository : EFRepository<客戶聯絡人>, I客戶聯絡人Repository
-	{
+{
+    public class 客戶聯絡人Repository : EFRepository<客戶聯絡人>, I客戶聯絡人Repository
+    {
         public override IQueryable<客戶聯絡人> All()
         {
             return base.All().Where(x => x.客戶資料.是否已刪除 != true && x.是否已刪除 != true);
@@ -17,6 +17,18 @@ namespace WebApplication3.Models
             return base.All().Where(x => x.客戶資料.是否已刪除 == true || x.是否已刪除 == true).Select(x => new { x.職稱, x.姓名, x.Email, x.手機, x.電話, x.客戶資料.客戶名稱 });
         }
 
+        public bool EmailDuplicate(int Id, int ClientId, string email)
+        {
+            if (Id != 0 && Find(Id).Email.Equals(email))
+            {
+                return false;
+            }
+
+            客戶聯絡人 聯絡人 = All().Where(x => x.客戶Id.Equals(ClientId)).FirstOrDefault(x => x.Email.Equals(email));
+            return 聯絡人 != null ? true : false;
+            
+        }
+
         public 客戶聯絡人 Find(int id)
         {
             return All().FirstOrDefault(x => x.Id.Equals(id));
@@ -25,7 +37,7 @@ namespace WebApplication3.Models
         public IQueryable<客戶聯絡人> Search(string Keyword, string classification)
         {
             var data = All();
-           
+
             if (!string.IsNullOrEmpty(Keyword))
             {
                 data = data.Where(x => x.Email.Contains(Keyword) || x.姓名.Contains(Keyword) || x.手機.Contains(Keyword) || x.電話.Contains(Keyword));
@@ -50,12 +62,12 @@ namespace WebApplication3.Models
 
         public IQueryable<String> DropDownList()
         {
-            return All().Select(x=>x.職稱).Distinct();
+            return All().Select(x => x.職稱).Distinct();
         }
     }
 
-	public  interface I客戶聯絡人Repository : IRepository<客戶聯絡人>
-	{
+    public interface I客戶聯絡人Repository : IRepository<客戶聯絡人>
+    {
 
-	}
+    }
 }
